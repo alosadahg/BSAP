f52fbcf14c885c97e12d3e9be8336efbc26cba9a
hg <hgalosada@gmail.com>
2023-09-29 19:51:08 +0800

    initial commit

---
+++ BSAPortal/Portal/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here   .
+
+
+class User(models.Model):
+    user_id = models.BigAutoField
+    username = models.CharField(max_length=25)
+
+class Admin(User):
+    def __str__(self):
+        return  self.username
+
+class Transaction(models.Model):
+    transactionID = models.BigAutoField
+    transaction_userid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
