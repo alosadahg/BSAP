0ab6777b00b2a2040026f1b58b74486a2edae45a
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-09-29 21:53:40 +0800

    Models Document Request 1.0

--- BSAPortal/DocumentRequest/models.py
+++ BSAPortal/DocumentRequest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.BigAutoField
+    username = models.CharField(max_length=25)
+
+
+class Resident(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    present_address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Document(models.Model):
+    doc_type = models.BigAutoField
+    res_name = models.ForeignKey(Resident, on_delete=models.CASCADE)
